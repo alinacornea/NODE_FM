4d0fb30191c953342f9ae69c85cd9dc72f29892f
alinacornea <alinacornea18@gmail.com>
2018-02-06 18:15:48 -0800

    commit

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var index = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'client/public/src'));
+app.set('view engine', 'jsx');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,6 +20,7 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', index);
 // app.use('/programs', filemaker);
